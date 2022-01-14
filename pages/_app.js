7f656e239d54df5d91d099ad01f04f3e6bf09655
Saransh Jain <saransh.jain@mail.utoronto.ca>
2022-01-14 00:03:43 -0500

    Initialize Chakra UI and data fetching.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      "body": {
+        backgroundColor: props.colorMode === "light" ? "gray.100" : "gray.900"
+      },
+    }),
+  },
+  fonts: {
+    body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+    heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    bold: 700
+  },
+  initialColorMode: "light",
+  useSystemColorMode: false
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-export default function Home() {
+export default function Home({ pictures }) {
   return (
     <div>
-      
+      {console.log(pictures)}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://api.nasa.gov/planetary/apod?api_key=" + process.env.NEXT_PUBLIC_NASA_API_KEY + "&start_date=2022-01-01");
+  const data = await res.json();
+
+  return {
+    props: { pictures: data }
+  }
 }
