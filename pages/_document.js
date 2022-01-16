f5b3c23ecbb2e3e4c8a6c7f5a2d96e2afc0aeba7
Saransh Jain <saransh.jain@mail.utoronto.ca>
2022-01-16 00:25:43 -0500

    Update favicon.

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,10 @@ export default class Document extends NextDocument {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />    
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Lobster&display=swap" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
